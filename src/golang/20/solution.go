ad4a85a5ad2c56588708db6aac2ddc4494cfd325
dhorn <dhorn@home.it>
2022-12-20 21:49:28 +0000

    aoc2020-20

--- src/golang/20/solution.go
+++ src/golang/20/solution.go
@@ -6,64 +6,85 @@ import (
 	"strconv"
 )
 
+type EncryptionValue struct {
+	CurrentPosition int
+	OffseValue      int64
+}
+
 func main() {
 
+	// Part2
+	p2decryptionKey := int64(811589153)
+	p2NumberOfMixes := 10
+
+	//Part 1 with Part2-Code
+	//p2decryptionKey := int64(1)
+	//p2NumberOfMixes := 1
+
 	initialSequence := aocutils.ReadInput("input.txt")
-	var newIndizes = make(map[int]int, len(initialSequence))
-	var newSequence = make([]int, len(initialSequence))
+	var newSequence = make([]*EncryptionValue, len(initialSequence))
+	var initialIndizes = make(map[int]*EncryptionValue)
 	//initialize new sequence
 
-	for index, _ := range initialSequence {
+	for index, value := range initialSequence {
 		//offset, _ := strconv.Atoi(offsetAsString)
-		newIndizes[index] = index
+		offset, _ := strconv.Atoi(value)
+		encryptionValue := &EncryptionValue{index, int64(offset) * p2decryptionKey}
+		newSequence[index] = encryptionValue
+		initialIndizes[index] = encryptionValue
 	}
 
 	//fmt.Println(newIndizes)
-	for initialIndex, offsetAsString := range initialSequence {
-		offset, _ := strconv.Atoi(offsetAsString)
-		rollOverSanitizedOffset := offset % len(initialSequence)
-		//fmt.Println(rollOverSanitizedOffset)
-		if rollOverSanitizedOffset < 0 {
-			rollOverSanitizedOffset = len(initialSequence) - 1 + rollOverSanitizedOffset
-		}
-		newIndex := newIndizes[initialIndex] + rollOverSanitizedOffset
+	for x := 0; x < p2NumberOfMixes; x++ {
+		for initialIndex, _ := range initialSequence {
 
-		if newIndex > len(initialSequence) {
-			newIndex = newIndex - (len(initialSequence) - 1)
-		}
+			encryptionValue := initialIndizes[initialIndex]
+			//fmt.Println(rollOverSanitizedOffset)
 
-		oldIndex := newIndizes[initialIndex]
+			newIndex := int64(encryptionValue.CurrentPosition) + encryptionValue.OffseValue
 
-		for x, _ := range initialSequence {
-			validateIndex := x
-			if x == initialIndex {
-				newIndizes[validateIndex] = newIndex
-				continue
+			for newIndex < 0 || newIndex >= int64(len(initialSequence)) {
+				if newIndex >= int64(len(initialSequence)) {
+					overflowCorrection := newIndex / int64(len(initialSequence))
+					newIndex = newIndex%int64(len(initialSequence)) + overflowCorrection
+				}
+				if newIndex < 0 {
+					overflowCorrection := newIndex/int64(len(initialSequence)) - 1
+					newIndex = int64(len(initialSequence)) + newIndex%int64(len(initialSequence)) + overflowCorrection
+				}
 			}
 
-			if newIndizes[validateIndex] < oldIndex && newIndizes[validateIndex] > newIndex {
-				newIndizes[validateIndex]++
-				continue
+			newIndexInt := int(newIndex)
+			oldIndex := encryptionValue.CurrentPosition
+
+			firstHalf := make([]*EncryptionValue, oldIndex)
+			secondHalf := make([]*EncryptionValue, len(newSequence)-1-oldIndex)
+
+			firstHalfNewIndex := make([]*EncryptionValue, newIndex)
+			secondHalfNewIndex := make([]*EncryptionValue, len(newSequence)-1-newIndexInt)
+
+			copy(firstHalf, newSequence[0:oldIndex])
+			copy(secondHalf, newSequence[oldIndex+1:len(newSequence)])
+
+			sliceWithoutElement := append(firstHalf, secondHalf...)
+
+			copy(firstHalfNewIndex, sliceWithoutElement[0:newIndex])
+			copy(secondHalfNewIndex, sliceWithoutElement[newIndex:len(sliceWithoutElement)])
+
+			firstHalfNewIndex = append(firstHalfNewIndex, []*EncryptionValue{encryptionValue}...)
+			newSequence = append(firstHalfNewIndex, secondHalfNewIndex...)
+			//fmt.Println(newSequence)
+			for newIndex, this := range newSequence {
+				this.CurrentPosition = newIndex
+				//fmt.Print(this.OffseValue, " ")
 			}
-			if newIndizes[validateIndex] > oldIndex && newIndizes[validateIndex] < newIndex {
-				newIndizes[validateIndex]--
-				continue
-			}
-
+			//fmt.Println()
 		}
-
-		for index, value := range initialSequence {
-			newIndex := newIndizes[index]
-			offset, _ := strconv.Atoi(value)
-			newSequence[newIndex] = offset
-		}
-
-		//fmt.Println(newSequence)
 	}
 
 	indexZero := 0
 	for index, value := range newSequence {
-		if value == 0 {
+		if value.OffseValue == 0 {
 			indexZero = index
 			break
 		}
@@ -71,7 +92,7 @@ func main() {
 
 	indizesToCheck := [3]int{(indexZero + 1000) % len(initialSequence), (indexZero + 2000) % len(initialSequence), (indexZero + 3000) % len(initialSequence)}
 	fmt.Println(indexZero, indizesToCheck)
-	fmt.Println(newSequence[indizesToCheck[0]], newSequence[indizesToCheck[1]], newSequence[indizesToCheck[2]], "=",
-		newSequence[indizesToCheck[0]]+newSequence[indizesToCheck[1]]+newSequence[indizesToCheck[2]])
+	fmt.Println(newSequence[indizesToCheck[0]].OffseValue, newSequence[indizesToCheck[1]].OffseValue, newSequence[indizesToCheck[2]].OffseValue, "=",
+		newSequence[indizesToCheck[0]].OffseValue+newSequence[indizesToCheck[1]].OffseValue+newSequence[indizesToCheck[2]].OffseValue)
 
 }
